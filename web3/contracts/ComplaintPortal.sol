cbc6611db0bdcf67df84e6b7c311fe40091bfc4b
nijin26 <nijinazarkpz@gmail.com>
2023-05-20 13:57:54 +0530

    Contract is refactored & access roles defined.

--- web3/contracts/ComplaintPortal.sol
+++ web3/contracts/ComplaintPortal.sol
@@ -46,8 +46,6 @@ contract ComplaintPortal is AccessControl {
         ComplaintDetailsByPolice policeComplaint;
     }
 
-    ComplaintEntry[] public complaints;
-
     struct Superior {
         string name;
         string email;
@@ -104,316 +102,86 @@ contract ComplaintPortal is AccessControl {
         RelativeDetails relativeDetails;
     }
 
-    address[] public addressList;
+    ComplaintEntry[] public complaints;
+    address[] public listOfUsers;
+    address[] public listOfStations;
+    address[] public listOfSuperiors;
 
-    mapping(address => uint) public ComplaintByID;
     mapping(address => Superior) public policeSuperiors;
     mapping(address => Station) public policeStations;
     mapping(address => ProfileInfo) public userProfiles;
 
-   function addComplaint(
-        uint _complaintId,
-        string memory _complaintNature,
-        string memory _complaintSubject,
-        string memory _complaintDescription,
-        string memory _dateAndTime,
-        string memory _placeOfIncident,
-        string memory _landmark,
-        string memory _district,
-        address _policeStation,
-        string memory _officeToFileComplaint,
-        address _witness,
-        address _accused,
-        string memory _ipc,
-        string memory _status,
-        string memory _remarks
-    ) public {
+    // <<<<<<<<<<<< COMPLAINT MODULE >>>>>>>>>>>>>>>>>>
+
+    function addComplaintByUser(
+        UserComplaintDetails memory _details
+    ) public onlyRole(USER) {
         ComplaintEntry memory entry;
-        entry.userComplaint = UserComplaintDetails(
-            _complaintNature,
-            _complaintSubject,
-            _complaintDescription,
-            _dateAndTime,
-            _placeOfIncident,
-            _landmark,
-            _district,
-            _policeStation,
-            _officeToFileComplaint
-        );
-        entry.policeComplaint = ComplaintDetailsByPolice(
-            _witness,
-            _accused,
-            _ipc,
-            _status,
-            _remarks
-        );
+        entry.userComplaint = _details;
         entry.complaintID = complaints.length; // complaintId is auto-incremented by array index
         complaints.push(entry);
     }
-}
 
-    // function addComplaint(ComplaintDetails memory complaint) public {
-    //     complaint.complaintId = complaints.length; // complaintId is auto-incremented by array index
-    //     complaints.push(complaint);
-
- function getComplaintDetailsByAddress(address _address) public view returns (UserComplaintDetails memory, ComplaintDetailsByPolice memory) {
-    for (uint i = 0; i < complaints.length; i++) {
-        if (complaints[i].userComplaint.policeStation == _address) {
-            return (complaints[i].userComplaint, complaints[i].policeComplaint);
-        }
-    }
-    // If no matching complaint is found, return empty structs or handle the case as per your requirement
-    revert("No complaint found for the given address");
-}
-
-
-function updateComplaint(uint complaintId, UserComplaintDetails memory updatedDetails, ComplaintDetailsByPolice memory updatedPoliceDetails) public {
-    require(complaintId < complaints.length, "Invalid complaint ID");
-
-    Complaint storage complaint = complaints[complaintId];
-
-    // Update user complaint details
-    complaint.userComplaint.complaintNature = updatedDetails.complaintNature;
-    complaint.userComplaint.complaintSubject = updatedDetails.complaintSubject;
-    complaint.userComplaint.complaintDescription = updatedDetails.complaintDescription;
-    complaint.userComplaint.dateAndTime = updatedDetails.dateAndTime;
-    complaint.userComplaint.placeOfIncident = updatedDetails.placeOfIncident;
-    complaint.userComplaint.landmark = updatedDetails.landmark;
-    complaint.userComplaint.district = updatedDetails.district;
-    complaint.userComplaint.policeStation = updatedDetails.policeStation;
-    complaint.userComplaint.officeToFileComplaint = updatedDetails.officeToFileComplaint;
-
-    // Update police complaint details
-    complaint.policeComplaint.witness = updatedPoliceDetails.witness;
-    complaint.policeComplaint.accused = updatedPoliceDetails.accused;
-    complaint.policeComplaint.ipc = updatedPoliceDetails.ipc;
-    complaint.policeComplaint.status = updatedPoliceDetails.status;
-    complaint.policeComplaint.remarks = updatedPoliceDetails.remarks;
-}
-
-
-    function createSuperiorProfile(
-        string memory _name,
-        string memory _email,
-        uint _mobile,
-        uint _aadharID,
-        string memory _rank,
-        string memory _designation,
-        string memory _unit
+    function addComplaintByPolice(
+        UserComplaintDetails memory _details,
+        ComplaintDetailsByPolice memory _otherDetails
     ) public {
-        policeSuperiors[msg.sender] = Superior({
-            name: _name,
-            email: _email,
-            mobile: _mobile,
-            aadharID: _aadharID,
-            rank: _rank,
-            designation: _designation,
-            unit: _unit,
-            approved: false,
-            approvedBy: address(0)
-        });
-    }
-
-    function addApprovedSuperiorProfile(
-        string memory _name,
-        string memory _email,
-        uint _mobile,
-        uint _aadharID,
-        string memory _rank,
-        string memory _designation,
-        string memory _unit,
-        address _newSuperior
-    ) public {
-        policeSuperiors[_newSuperior] = Superior({
-            name: _name,
-            email: _email,
-            mobile: _mobile,
-            aadharID: _aadharID,
-            rank: _rank,
-            designation: _designation,
-            unit: _unit,
-            approved: true,
-            approvedBy: msg.sender
-        });
-    }
-
-    function updateSuperiorProfile(
-        string memory _name,
-        string memory _email,
-        uint _mobile,
-        uint _aadharID,
-        string memory _rank,
-        string memory _designation,
-        string memory _unit
-    ) public {
-        Superior storage profile = policeSuperiors[msg.sender];
-        profile.name = _name;
-        profile.email = _email;
-        profile.mobile = _mobile;
-        profile.aadharID = _aadharID;
-        profile.rank = _rank;
-        profile.designation = _designation;
-        profile.unit = _unit;
-        // emit ProfileUpdated(msg.sender);
-    }
-
-    function approvePoliceSuperior(
-        address _policeSuperior,
-        bool _approved
-    ) public {
-        policeSuperiors[_policeSuperior].approved = _approved;
-        policeSuperiors[_policeSuperior].approvedBy = msg.sender;
-        // emit ApprovalUpdated(_policeSuperior, _approved);
-    }
-
-    function getSuperiorProfileDetails()
-        public
-        view
-        returns (
-            string memory,
-            string memory,
-            uint,
-            uint,
-            string memory,
-            string memory,
-            string memory,
-            bool,
-            address
-        )
-    {
-        Superior storage profile = policeSuperiors[msg.sender];
-        return (
-            profile.name,
-            profile.email,
-            profile.mobile,
-            profile.aadharID,
-            profile.rank,
-            profile.designation,
-            profile.unit,
-            profile.approved,
-            profile.approvedBy
-        );
-    }
-
-    function getApprovingSuperiorDetails(
-        address _approvedBy
-    ) public view returns (string memory name, string memory designation) {
-        require(_approvedBy != address(0), "Invalid address");
+        // Check authorization
         require(
-            policeSuperiors[_approvedBy].approved,
-            "Not an approved superior"
+            hasRole(STATION, msg.sender) || hasRole(SUPERIOR, msg.sender),
+            "YOU ARE NOT AUTHORIZED"
         );
 
-        return (
-            policeSuperiors[_approvedBy].name,
-            policeSuperiors[_approvedBy].designation
-        );
+        ComplaintEntry memory entry;
+        entry.complaintID = complaints.length; // complaintId is auto-incremented by array index
+        entry.userComplaint = _details;
+        entry.policeComplaint = _otherDetails;
+        complaints.push(entry);
     }
 
-    function createStationProfile(
-        string memory _name,
-        string memory _addr,
-        string memory _district,
-        string memory _landmark,
-        string memory _stationType,
-        uint _mobile,
-        string memory _nameOfCI,
-        string memory _nameOfSI
-    ) public {
-        policeStations[msg.sender] = Station({
-            name: _name,
-            addr: _addr,
-            district: _district,
-            landmark: _landmark,
-            stationType: _stationType,
-            mobile: _mobile,
-            nameOfCI: _nameOfCI,
-            nameOfSI: _nameOfSI,
-            approved: false,
-            approvedBy: address(0)
-        });
-        addressList.push(msg.sender); // Add the new police station address to the addressList array
-    }
-
-    function updateStationProfile(
-        string memory _name,
-        string memory _addr,
-        string memory _district,
-        string memory _landmark,
-        string memory _stationType,
-        uint _mobile,
-        string memory _nameOfCI,
-        string memory _nameOfSI
-    ) public {
-        Station storage profile = policeStations[msg.sender];
-        profile.name = _name;
-        profile.addr = _addr;
-        profile.district = _district;
-        profile.landmark = _landmark;
-        profile.stationType = _stationType;
-        profile.mobile = _mobile;
-        profile.nameOfCI = _nameOfCI;
-        profile.nameOfSI = _nameOfSI;
-        profile.addr = _addr;
-        profile.mobile = _mobile;
-        // emit StationProfileUpdated(msg.sender);
-    }
-
-    function approveStationProfile(
-        address _policeStation,
-        bool _approved
-    ) public onlyRole(SUPERIOR) {
-        policeStations[_policeStation].approved = _approved;
-        policeStations[_policeStation].approvedBy = msg.sender;
-        // emit StationApprovalUpdated(_policeStation, _approved);
-    }
-
-    function getStationDetails()
+    //<<<<<<<<< GET COMPLAINT DETAILS >>>>>>>>>>>>>>>
+    function getComplaintDetailsById(
+        uint _complaintId
+    )
         public
         view
-        returns (
-            string memory,
-            string memory,
-            string memory,
-            string memory,
-            string memory,
-            uint,
-            string memory,
-            string memory,
-            bool,
-            address
-        )
+        returns (UserComplaintDetails memory, ComplaintDetailsByPolice memory)
     {
-        Station memory profile = policeStations[msg.sender];
-        return (
-            profile.name,
-            profile.addr,
-            profile.district,
-            profile.landmark,
-            profile.stationType,
-            profile.mobile,
-            profile.nameOfCI,
-            profile.nameOfSI,
-            profile.approved,
-            profile.approvedBy
+        require(_complaintId < complaints.length, "Invalid complaint ID");
+
+        ComplaintEntry storage complaint = complaints[_complaintId];
+        return (complaint.userComplaint, complaint.policeComplaint);
+    }
+
+    // <<<<<<<<< UPDATE COMPLAINT >>>>>>>>>>>>
+    function updateComplaintByUser(
+        uint _complaintId,
+        UserComplaintDetails memory _updatedDetails
+    ) public onlyRole(USER) {
+        require(_complaintId < complaints.length, "Invalid complaint ID");
+
+        ComplaintEntry storage complaint = complaints[_complaintId];
+        complaint.userComplaint = _updatedDetails;
+    }
+
+    function updateComplaintByPolice(
+        uint complaintId,
+        UserComplaintDetails memory updatedDetails,
+        ComplaintDetailsByPolice memory updatedPoliceDetails
+    ) public {
+        require(complaintId < complaints.length, "Invalid complaint ID");
+        require(
+            hasRole(STATION, msg.sender) || hasRole(SUPERIOR, msg.sender),
+            "YOU ARE NOT AUTHORIZED"
         );
+
+        ComplaintEntry storage complaint = complaints[complaintId];
+        complaint.userComplaint = updatedDetails;
+        complaint.policeComplaint = updatedPoliceDetails;
     }
 
-    function getAllPoliceStations() public view returns (Station[] memory) {
-        uint count = 0;
-        for (uint i = 0; i < addressList.length; i++) {
-            count++;
-        }
-        Station[] memory result = new Station[](count);
-        uint index = 0;
-        for (uint i = 0; i < addressList.length; i++) {
-            result[index] = policeStations[addressList[i]];
-            index++;
-        }
-        return result;
-    }
+    // <<<<<<<<<<<<< USER MODULE >>>>>>>>>>>>
 
-    // Function to create a new user
     function createUser(ProfileInfo memory profileInfo) public {
         userProfiles[msg.sender] = profileInfo;
     }
@@ -422,7 +190,130 @@ function updateComplaint(uint complaintId, UserComplaintDetails memory updatedDe
         return userProfiles[msg.sender];
     }
 
-    function updateUserDetails(ProfileInfo memory profileInfo) public {
+    function updateUserDetails(
+        ProfileInfo memory profileInfo
+    ) public onlyRole(USER) {
         userProfiles[msg.sender] = profileInfo;
+    }
+
+    // <<<<<<<<< SUPERIOR MODULE >>>>>>>>>>>>>>>
+
+    function createSuperiorProfile(Superior memory profileInfo) public {
+        policeSuperiors[msg.sender] = profileInfo;
+    }
+
+    function updateSuperiorProfile(Superior memory profileInfo) public {
+        policeSuperiors[msg.sender] = profileInfo;
+    }
+
+    function addApprovedSuperiorProfile(
+        address _newSuperior,
+        Superior memory profileInfo
+    ) public onlyRole(OWNER) {
+        policeSuperiors[_newSuperior] = profileInfo;
+    }
+
+    function approvePoliceSuperior(address _policeSuperior) public {
+        require(
+            hasRole(OWNER, msg.sender) || hasRole(SUPERIOR, msg.sender),
+            "YOU ARE NOT AUTHORIZED"
+        );
+        policeSuperiors[_policeSuperior].approved = true;
+        policeSuperiors[_policeSuperior].approvedBy = msg.sender;
+    }
+
+    function getSuperiorProfileDetails()
+        public
+        view
+        onlyRole(SUPERIOR)
+        returns (Superior memory)
+    {
+        Superior storage profile = policeSuperiors[msg.sender];
+        return (profile);
+    }
+
+    function getApprovingSuperiorDetails(
+        address _approvedBy
+    ) public view returns (string memory name, string memory designation) {
+        require(_approvedBy != address(0), "Invalid address");
+        require(hasRole(SUPERIOR, _approvedBy), "Not an approved superior");
+
+        return (
+            policeSuperiors[_approvedBy].name,
+            policeSuperiors[_approvedBy].designation
+        );
+    }
+
+    function createStationProfile(Station memory _stationDetails) public {
+        policeStations[msg.sender] = _stationDetails;
+    }
+
+    function updateStationProfile(
+        Station memory _updatedStationDetails
+    ) public onlyRole(STATION) {
+        policeStations[msg.sender] = _updatedStationDetails;
+    }
+
+    function approveStationProfile(
+        address _policeStation
+    ) public onlyRole(SUPERIOR) {
+        policeStations[_policeStation].approved = true;
+        policeStations[_policeStation].approvedBy = msg.sender;
+    }
+
+    function getStationDetails() public view returns (Station memory) {
+        Station memory profile = policeStations[msg.sender];
+        return (profile);
+    }
+
+    function getAllUsers()
+        public
+        view
+        onlyRole(SUPERIOR)
+        returns (ProfileInfo[] memory)
+    {
+        uint userCount = listOfUsers.length;
+        ProfileInfo[] memory result = new ProfileInfo[](userCount);
+
+        uint index = 0;
+        for (uint i = 0; i < userCount; i++) {
+            result[index] = userProfiles[listOfUsers[i]];
+            index++;
+        }
+        return result;
+    }
+
+    function getAllSuperiors()
+        public
+        view
+        onlyRole(SUPERIOR)
+        returns (Superior[] memory)
+    {
+        uint superiorCount = listOfSuperiors.length;
+        Superior[] memory result = new Superior[](superiorCount);
+
+        uint index = 0;
+        for (uint i = 0; i < superiorCount; i++) {
+            result[index] = policeSuperiors[listOfSuperiors[i]];
+            index++;
+        }
+        return result;
+    }
+
+    function getAllPoliceStations()
+        public
+        view
+        onlyRole(SUPERIOR)
+        returns (Station[] memory)
+    {
+        uint stationCount = listOfStations.length;
+        Station[] memory result = new Station[](stationCount);
+
+        uint index = 0;
+        for (uint i = 0; i < stationCount; i++) {
+            result[index] = policeStations[listOfStations[i]];
+            index++;
+        }
+        return result;
     }
 }
