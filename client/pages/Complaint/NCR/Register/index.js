f79c5b2bd4333a9893eb7e1511698cbdaf127a9d
nijin26 <nijinazarkpz@gmail.com>
2023-06-18 12:11:18 +0530

    Add complainant's details section in NCR registration page

--- client/pages/Complaint/NCR/Register/index.js
+++ client/pages/Complaint/NCR/Register/index.js
@@ -3,6 +3,31 @@ import Button from "@/Components/Button";
 
 const RegisterNCR = () => {
   const [selectedTab, setSelectedTab] = useState(1);
+  const profileInfo = {
+    basicDetails: {
+      name: "John Doe",
+      email: "johndoe@example.com",
+      mobile: 1234567890,
+      age: 25,
+      gender: "Male",
+      dob: "1998-05-10",
+      addr: "123 Main Street",
+      city: "New York",
+      district: "ABC District",
+      state: "New York",
+      pincode: 12345,
+    },
+    idDetails: {
+      selectedID: "Driving License",
+      IDNumber: "DL-123456789",
+    },
+    relativeDetails: {
+      relativeName: "Jane Doe",
+      relativeMobile: 9876543210,
+      relation: "Other",
+    },
+  };
+
   const [selectedComplaint, setSelectedComplaint] = useState({
     userAddress: "",
     userName: "",
@@ -152,10 +177,66 @@ const RegisterNCR = () => {
             <h1 className="my-4 font-bold text-center text-xl">
               Complainant's Details
             </h1>
-            <p>
-              <span className="font-bold">Complainant's Name</span>{" "}
-              {selectedComplaint.remarks}
-            </p>
+            <div>
+              {" "}
+              <p>
+                <strong>Name:</strong> {profileInfo.basicDetails.name}
+              </p>
+              <p>
+                <strong>Email:</strong> {profileInfo.basicDetails.email}
+              </p>
+              <p>
+                <strong>Mobile:</strong> {profileInfo.basicDetails.mobile}
+              </p>
+              <p>
+                <strong>Age:</strong> {profileInfo.basicDetails.age}
+              </p>
+              <p>
+                <strong>Gender:</strong> {profileInfo.basicDetails.gender}
+              </p>
+              <p>
+                <strong>Date of Birth:</strong> {profileInfo.basicDetails.dob}
+              </p>
+              <p>
+                <strong>Address:</strong> {profileInfo.basicDetails.addr}
+              </p>
+              <p>
+                <strong>City:</strong> {profileInfo.basicDetails.city}
+              </p>
+              <p>
+                <strong>District:</strong> {profileInfo.basicDetails.district}
+              </p>
+              <p>
+                <strong>State:</strong> {profileInfo.basicDetails.state}
+              </p>
+              <p>
+                <strong>Pincode:</strong> {profileInfo.basicDetails.pincode}
+              </p>
+            </div>
+            <div className="mt-4 space-y-4">
+              <h3 className="font-bold text-stone-500">ID Details</h3>
+              <p>
+                <strong>Selected ID:</strong> {profileInfo.idDetails.selectedID}
+              </p>
+              <p>
+                <strong>ID Number:</strong> {profileInfo.idDetails.IDNumber}
+              </p>
+            </div>
+            <div className="mt-4 space-y-4">
+              <h3 className="font-bold text-stone-500">Relative Details</h3>
+              <p>
+                <strong>Relative Name:</strong>{" "}
+                {profileInfo.relativeDetails.relativeName}
+              </p>
+              <p>
+                <strong>Relative Mobile:</strong>{" "}
+                {profileInfo.relativeDetails.relativeMobile}
+              </p>
+              <p>
+                <strong>Relation:</strong>{" "}
+                {profileInfo.relativeDetails.relation}
+              </p>
+            </div>
           </div>
         )}
       </div>
